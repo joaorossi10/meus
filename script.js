8442903271a0e0140050be182f48aff83658d4da
joao vitor fernandes <108941517+joaorossi10@users.noreply.github.com>
2024-06-05 09:45:35 -0300

    05/06

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textosResultado = document.querySelector(".textos-resultado")
+
+const perguntas =[
+{
+    enunciado: "Pergunta1"
+    alternantivas["Alternativa1", "Alternativa2"]
+},
+{ 
+        enunciado: "Pergunta1"
+        alternantivas["Alternativa1", "Alternativa2"]
+},
+]
